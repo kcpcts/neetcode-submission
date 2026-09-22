62e4565ff9d5d1efbb0abaae1c2e2c8152d800ee
Krish Chauhan <100506108+kcpcts@users.noreply.github.com>
2026-09-22 11:44:34 -0700

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest = [None,None,-1]
+    # start at some character i
+    # left,right = i
+    # if left-1 and right+1 are equal: (expand palindrome)
+    # left -= 1
+    # right += 1
+    # update running maximum with s[i:j+1]
+    # recurse with new left and right
+    # else:
+    # return
+
+    # if 
+        def check(left,right):
+            if left < 0 or right >= len(s):
+                return
+            if left == right:
+                check(left-1,left)
+            
+            if s[left] == s[right]:
+                if (right-left + 1) > longest[2]:
+                    longest[0], longest[1] = left, right 
+                    longest[2] = (right-left + 1)
+                check(left-1,right+1)
+            
+
+
+        for i,char in enumerate(s):
+            check(i,i)
+        return s[longest[0]:longest[1]+1]
+
+
